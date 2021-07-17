7871a5e334bbb5d327ac2eb0cb233b25f72081ba
“yusvkekim” <planetaryg0812@icloud.com>
2021-07-17 16:49:51 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kitchentimer2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kitchentimer2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView mTimerText;
+    ImageView mStartButton;
+    ImageView mStopButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTimerText = (TextView) findViewById(R.id.timer_text);
+        mStartButton = (ImageView) findViewById(R.id.timer_start);
+        mStopButton = (ImageView) findViewById(R.id.timer_stop);
+    }
+}
