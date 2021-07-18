f0ecad3fd486a600c429d3c5a48c9b297ebb0d4a
“yusvkekim” <planetaryg0812@icloud.com>
2021-07-18 16:17:46 +0900

    second commit(計算式、タイマーボタン押下時の設定)

--- app/src/main/java/com/example/kitchentimer2/MainActivity.java
+++ app/src/main/java/com/example/kitchentimer2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.kitchentimer2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,5 +22,41 @@ public class MainActivity extends AppCompatActivity {
         mTimerText = (TextView) findViewById(R.id.timer_text);
         mStartButton = (ImageView) findViewById(R.id.timer_start);
         mStopButton = (ImageView) findViewById(R.id.timer_stop);
+
+        final CountDownTimer timer = new CountDownTimer(3*60*1000, 100) {
+
+            //指定した間隔で実行したい処理
+            @Override
+            public void onTick(long l) {
+                long minute = l /1000 / 60;
+                long second = l /1000 % 60;
+
+                //フォーマットをかける
+                mTimerText.setText(String.format("%1$d:%2$02d", minute, second));
+            }
+
+            //タイマー終了時の処理
+            @Override
+            public void onFinish() {
+
+                mTimerText.setText("0:00");
+
+            }
+        };
+
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.start();
+            }
+        });
+
+        mStopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                timer.cancel();
+            }
+        });
+
     }
 }
